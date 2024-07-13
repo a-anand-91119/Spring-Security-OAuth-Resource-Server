ef9aaa7ec3e7cf4841a4c4e2faa9b3b1162d3bf2
A Anand <a.anand.91119@gmail.com>
2024-07-13 13:07:16 +0530

    Initial commit

---
+++ src/main/java/dev/notyouraverage/spring_security_oauth_resource_server/SpringSecurityOauthResourceServerApplication.java
@@ -0,0 +1,13 @@
+package dev.notyouraverage.spring_security_oauth_resource_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauthResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/notyouraverage/spring_security_oauth_resource_server/SpringSecurityOauthResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.notyouraverage.spring_security_oauth_resource_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
